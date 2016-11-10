64bb90d5931f57e94a1b31872595f88d1e24b2ef
baochunyu <baochunyu@nonobank.com>
2016-11-10 11:19:13 +0800

    init

---
+++ ms-turbine/src/test/java/com/bao/MsTurbineApplicationTests.java
@@ -0,0 +1,19 @@
+package com.bao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsTurbineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
